8f617d17f6e148a3802292ff5763e4df274bad57
Alexandr <nasanovicha@gmail.com>
2026-06-10 12:38:23 +0300

    products section was enhanced

--- components/sections/ProductsSection/ProductCardItem.tsx
+++ components/sections/ProductsSection/ProductCardItem.tsx
@@ -5,11 +5,19 @@ type TProductCardItem = {
 };
 
 export const ProductCardItem = ({ label }: TProductCardItem) => {
-  const checkIcon = ICONS["check"]("size-4 me-2 inline-block text-success");
+  const checkIcon = ICONS["check"](
+    "size-4 shrink-0 text-(--brand-blue) mt-0.5",
+  );
+
   return (
-    <li>
-      {checkIcon}
-      <span>{label}</span>
+    <li className="flex items-start gap-2">
+      <span className="sr-only">Преимущество:</span>
+
+      <div aria-hidden="true" className="flex shrink-0">
+        {checkIcon}
+      </div>
+
+      <span className="text-slate-600 leading-tight">{label}</span>
     </li>
   );
 };
